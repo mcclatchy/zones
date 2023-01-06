f9db3fe81e9732aa34624b0a7a3e568851080c00
Jay Pilgreen <jaypilgreen@gmail.com>
2023-01-06 13:26:31 -0600

    first attempt at story zones

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * Zones extension (Yozons)
  */
 
-import section from "./lib/section.js";
 import * as zones from "./lib/zones.js";
+import section from "./lib/section.js";
+import story from "./lib/story.js";
 
 function distributeZones(locker) {
   return new Promise((resolve, reject) => {
@@ -12,6 +13,9 @@ function distributeZones(locker) {
     switch(locker.pageType) {
       case "sectfront":
         map = section(locker);
+        break;
+      case "story":
+        map = story(locker);
         break;
       default:
         // do nothing
@@ -25,7 +29,7 @@ function distributeZones(locker) {
     } else {
       reject("zone map is empty");
     }
-  })
+  });
 }
 
 export default distributeZones;
